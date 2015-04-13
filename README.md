9d88050bce1d19e1ef585e75832dd8d383b1aace
Nikolay Mihaylov <nmmm@nmmm.nu>
2015-04-14 00:29:18 +0300

    reorganisation, refactor, add more optimization, add zoom

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Mandelbrot
 Mandelbrot set in C
 
 There is no makefile, to compile just do:
-> gcc mandelbrot.c
+> gcc mandelbrot_original.c
 
 This will produce file called ./a.out
 
@@ -17,6 +17,10 @@ You can see the image online here:
 
 https://www.flickr.com/photos/nmmmnu/16892543768/
 
+and here you can see some zoomed images:
+
+https://www.flickr.com/photos/nmmmnu/sets/72157651532271190/
+
 You can play with resolution (**MAX**) and zoom, just make sure there is no int overflow in **uint** type.
 
 Increasing **ITER** will increase colors, but 16 or 32 colors give best results. I did not tried over 255 colors, but there are no reason not to work, you need just to increase **color** type.
@@ -26,4 +30,23 @@ Distribution is GPL.
 Nikolay Mihaylov
 
 2015-04-08
+
+
+
+Update 2015-04-14
+=================
+
+I kept original version, because is easy to understand how it works,
+but I made "optimized" version that is faster, uses no array, is able to zoom and have different coloring technics.
+
+Final goal of "optimized" version will be to make fractal zoom movie using ffmpeg.
+
+Try play with defines just before main() function.
+
+Distribution is GPL.
+
+Nikolay Mihaylov
+
+2015-04-14
+
 
--- mandelbrot.c
+++ mandelbrot.c
@@ -1,41 +1,51 @@
 #include <stdio.h>
-#include <string.h>
 
-#define MAX 600
-
-#define ITER 32
-
-#define mmax ((double) 2.0) /* modul of mandelbrot escape */
-
-typedef unsigned char	color;
 typedef unsigned int	uint;
+typedef unsigned char	color;
 
-color m[MAX][MAX];
+typedef struct _Mandelbrot{
+	uint	scrx;
+	uint	scry;
 
-inline void clr(){
-	memset(m, 0, sizeof(m));
+	double	x;
+	double	y;
+
+	double	resx;
+	double	resy;
+
+	color	iter;
+} Mandelbrot;
+
+#define ESCAPE2 4.0 /* square of mandelbrot escape */
+
+static inline double getResolution(double screen, double width){
+	return width / (screen - 1);
 }
 
-inline double convert(uint a){
-	const double p = (mmax * 2)  / (MAX - 1);
-
-	return p * a - mmax;
+static inline double convertX(const Mandelbrot *m, uint x){
+	return m->x + m->resx * x;
 }
 
-color Z(double x, double y){
+static inline double convertY(const Mandelbrot *m, uint y){
+	return m->y + m->resy * y;
+}
+
+static inline color convertColor(const Mandelbrot *m, color c);
+
+static color Z(const Mandelbrot *m, double x, double y){
 	double zx = 0;
 	double zy = 0;
-		
+
 	color i;
-	for(i = 0; i < ITER; ++i){
+	for(i = 0; i < m->iter; ++i){
 		double zx2 = zx * zx;
 		double zy2 = zy * zy;
-		
-		if (zx2 + zy2 > 4)
+
+		if (zx2 + zy2 > ESCAPE2)
 			return i;
 
 		// z = z*z + c
-		
+
 		zy = 2 * zx * zy + y;
 		zx = zx2 - zy2 + x;
 	}
@@ -43,64 +53,103 @@ color Z(double x, double y){
 	return i;
 }
 
-void calc(){
-	clr();
-
-	uint x, y;
-	
-	for(x = 0; x < MAX; ++x){
-		double xr = convert(x);
-		for(y = 0; y < MAX; ++y){
-			double yr = convert(y);
-			m[x][y] = Z(xr, yr);
-		}
-	}
-}
-
-inline char tochar(color a){
-	switch(a){
-		case  0: return 'X';
-		case  1: return 'Z';
-		case  2: return '3';
-		case  3: return '4';
-		case  4: return '5';
-		case  5: return '6';
-		case  6: return '7';
-		case  7: return '-';
-		case  8: return ':';
-		case  9: return '.';
-		case 10: return ' ';
-	}
-
-	return ' ';
-}
-
-void show(){
+static void show(const Mandelbrot *m){
 	printf("P2\n");
-	printf("%u %u\n", MAX, MAX);
-	printf("%u\n", ITER);
+	printf("%u %u\n", m->scrx, m->scry);
+	printf("%u\n", m->iter);
 
 	uint x, y;
-	
-	for(y = 0; y < MAX; ++y){
-		for(x = 0; x < MAX; ++x){
-			printf("%u ", ITER - m[x][y]);
+
+	for(y = 0; y < m->scry; ++y){
+		double yr = convertY(m, y);
+		for(x = 0; x < m->scrx; ++x){
+			double xr = convertX(m, x);
+			color c = Z(m, xr, yr);
+
+			printf("%2u ", convertColor(m, c));
 		}
-		
+
 		printf("\n");
 	}
 }
 
-#define printfconvert(a)	printf("%3u : %10.4f\n",       a, convert(a));
+static const Mandelbrot *getMandelbrot(
+		uint scrx,      uint scry,
+		double centerx, double centery,
+		double half_widthx,
+		color iter
+	){
+
+	double half_widthy = half_widthx * scry / scrx;
+
+	static Mandelbrot m;
+
+	m.scrx = scrx;
+	m.scry = scry;
+
+	m.x = centerx - half_widthx;
+	m.y = centery - half_widthy;
+
+	m.resx = getResolution(scrx, half_widthx * 2);
+	m.resy = m.resx;
+
+	m.iter = iter;
+
+	return & m;
+}
+
+//#define SCREEN	  40,   40
+//#define SCREEN	 800,  600
+//#define SCREEN	1024,  768
+#define SCREEN	1280, 1024
+
+#define ITERATIONS	255
+//#define ITERATIONS	128
+//#define ITERATIONS	64
+
+#define _convertColor	convertColorClassic
+//#define _convertColor	convertColorCosmos
+//#define _convertColor	convertColorCosmosMix
+//#define _convertColor	convertColorNone
+
+
+#define _getMandelbrot(x, y, zoom)  getMandelbrot(SCREEN, x, y, zoom, ITERATIONS)
 
 int main(){
-//	printfconvert(0);
-//	printfconvert((MAX - 1) / 2);
-//	printfconvert(MAX - 1);
+	const Mandelbrot *m;
 
-	calc();
-	
-	show();
-	
+//	m = _getMandelbrot(-0.500, +0.000, 1.800);	// BIG Picture
+//	m = _getMandelbrot(-1.770, +0.000, 0.060);	// Bulb Mandelbrot
+	m = _getMandelbrot(+0.336, +0.052, 0.012);	// Elephant valley
+//	m = _getMandelbrot(-0.747, +0.102, 0.005);	// Seahorse valley
+//	m = _getMandelbrot(-0.041, +0.682, 0.008);	// Triple spiral
+//	m = _getMandelbrot(-1.370, +0.040, 0.018);	// Country valley
+
+	show(m);
+
 	return 0;
+}
+
+
+
+inline color convertColorClassic(const Mandelbrot *m, color c){
+	return m->iter - c;
+}
+
+inline color convertColorCosmos(const Mandelbrot *m, color c){
+	return c & 1 ? m->iter : 0;
+}
+
+inline color convertColorCosmosMix(const Mandelbrot *m, color c){
+	return c & 1 ? convertColorClassic(m, c) : 0;
+}
+
+inline color convertColorNone(const Mandelbrot *m, color c){
+	return c < m->iter ? m->iter : 0;
+}
+
+
+
+static inline color convertColor(const Mandelbrot *m, color c){
+	return _convertColor(m, c);
 }
