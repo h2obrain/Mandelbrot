e4e090711852a28e32d3164a9ac86f83a5f29051
Nikolay Mihaylov <nmmm@nmmm.nu>
2015-04-15 22:36:24 +0300

    add Perpendicular Burning ship, refactor types

--- mandelbrot.c
+++ mandelbrot.c
@@ -1,55 +1,67 @@
 #include <stdio.h>
 
-typedef unsigned int	uint;
-typedef unsigned char	color;
+#define uint	unsigned int
+#define color	unsigned short int
+#define float	double
 
 typedef struct _Mandelbrot{
 	uint	scrx;
 	uint	scry;
 
-	double	x;
-	double	y;
+	float	x;
+	float	y;
 
-	double	resx;
-	double	resy;
+	float	resx;
+	float	resy;
 
 	color	iter;
 
-	int	absolute;
+	char	absolute;
 } Mandelbrot;
 
 #define ESCAPE2 4.0 /* square of mandelbrot escape */
 
-static inline double getResolution(double screen, double width){
+static inline float getResolution(float screen, float width){
 	return width / (screen - 1);
 }
 
-static inline double convertX(const Mandelbrot *m, uint x){
+static inline float convertX(const Mandelbrot *m, uint x){
 	return m->x + m->resx * x;
 }
 
-static inline double convertY(const Mandelbrot *m, uint y){
+static inline float convertY(const Mandelbrot *m, uint y){
 	return m->y + m->resy * y;
 }
 
 static inline color convertColor(const Mandelbrot *m, color c);
 
-#define ABSF(a) (a >= 0 ? a : -a)
+static inline float absf(float a){
+	return a >= 0 ? a : -a;
+}
 
-static color Z(const Mandelbrot *m, double x, double y){
-	double zx = 0;
-	double zy = 0;
+static color Z(const Mandelbrot *m, float x, float y){
+	float zx = 0;
+	float zy = 0;
 
 	color i;
 	for(i = 0; i < m->iter; ++i){
-		if (m->absolute){
+		if (m->absolute == 1){
 			// convert the fractal to burning ship
-			zx = ABSF(zx);
-			zy = ABSF(zy);
+			if (zx < 0)
+				zx = -zx;
+
+			if (zy < 0)
+				zy = -zy;
 		}
 
-		double zx2 = zx * zx;
-		double zy2 = zy * zy;
+		if (m->absolute == 2){
+			// convert the fractal to perpendicular burning ship
+			if (zy < 0)
+				zy = -zy;
+		}
+
+		float zx2 = zx * zx;
+		float zy2 = zy * zy;
 
 		if (zx2 + zy2 > ESCAPE2)
 			return i;
@@ -71,9 +83,9 @@ static void show(const Mandelbrot *m){
 	uint x, y;
 
 	for(y = 0; y < m->scry; ++y){
-		double yr = convertY(m, y);
+		float yr = convertY(m, y);
 		for(x = 0; x < m->scrx; ++x){
-			double xr = convertX(m, x);
+			float xr = convertX(m, x);
 			color c = Z(m, xr, yr);
 
 			printf("%2u ", convertColor(m, c));
@@ -85,13 +97,13 @@ static void show(const Mandelbrot *m){
 
 static const Mandelbrot *getMandelbrot(
 		uint scrx,      uint scry,
-		double centerx, double centery,
-		double half_widthx,
+		float centerx, float centery,
+		float half_widthx,
 		color iter,
 		int absolute
 	){
 
-	double half_widthy = half_widthx * scry / scrx;
+	float half_widthy = half_widthx * scry / scrx;
 
 	static Mandelbrot m;
 
@@ -116,9 +128,12 @@ static const Mandelbrot *getMandelbrot(
 //#define SCREEN	1024,  768
 #define SCREEN	1280, 1024
 
+//#define ITERATIONS	1024
+//#define ITERATIONS	512
 #define ITERATIONS	255
 //#define ITERATIONS	128
 //#define ITERATIONS	64
+//#define ITERATIONS	32
 
 #define _convertColor	convertColorClassic
 //#define _convertColor	convertColorCosmos
@@ -128,6 +143,7 @@ static const Mandelbrot *getMandelbrot(
 
 #define _getMandelbrot(x, y, zoom)	getMandelbrot(SCREEN, x, y, zoom, ITERATIONS, 0)
 #define _getBurningShip(x, y, zoom)	getMandelbrot(SCREEN, x, y, zoom, ITERATIONS, 1)
+#define _getPerpendBS(x, y, zoom)	getMandelbrot(SCREEN, x, y, zoom, ITERATIONS, 2)
 
 int main(){
 	const Mandelbrot *m;
@@ -138,7 +154,20 @@ int main(){
 //	m = _getMandelbrot(-0.747, +0.102, 0.005);	// Seahorse valley
 //	m = _getMandelbrot(-0.041, +0.682, 0.008);	// Triple spiral
 //	m = _getMandelbrot(-1.370, +0.040, 0.018);	// Imperial Orb valley
-	m = _getBurningShip(-1.760, -0.040, 0.060);	// Burning ship
+
+//	m = _getBurningShip(-0.500, -0.500, 1.800);	// Burning ship, BIG Picture
+//	m = _getBurningShip(-1.760, -0.040, 0.060);	// Burning ship
+//	m = _getBurningShip(-1.772, -0.040, 0.010);	// Burning ship antenna detail
+//	m = _getBurningShip( 0.000, -1.015, 0.015);	// Burning ship mysterious lady
+
+//	m = _getPerpendBS(-0.50000, -0.50000, 1.80000);	// Perpendicular Burning ship, BIG Picture
+//	m = _getPerpendBS(-0.06990, +0.64980, 0.00040);	// Perpendicular Burning ship, IFS Tree Cardioid
+//	m = _getPerpendBS(-0.07950, +0.65900, 0.00050);	// Perpendicular Burning ship, Humanoid creature - The Alien
+//	m = _getPerpendBS(-0.08183, +0.64945, 0.00005);	// Perpendicular Burning ship, Humanoid creature - The Minotaur
+//	m = _getPerpendBS(-0.09675, +0.65246, 0.00005);	// Perpendicular Burning ship, Humanoid creature - The Skull
+//	m = _getPerpendBS(-0.07400, +0.65071, 0.00030);	// Perpendicular Burning ship, Spiral galaxy (set iterations to 1024)
+//	m = _getPerpendBS(-0.23218, +0.70837, 0.00100);	// Perpendicular Burning ship, Butterfly "BIG" Picture
+	m = _getPerpendBS(-0.23218, +0.70837, 0.00005);	// Perpendicular Burning ship, Butterfly
 
 	show(m);
 
