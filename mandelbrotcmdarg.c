4ef2b0fbab875764ecf92e10b6494b9013e79a52
Nikolay Mihaylov <nmmm@nmmm.nu>
2015-04-21 09:41:02 +0300

    refactor examples in mandelbrotcmdarg.c

--- mandelbrotcmdarg.c
+++ mandelbrotcmdarg.c
@@ -2,72 +2,87 @@
 
 #include "libmandelbrot.h"
 
-void printExamples(const char *p){
-	static const char *mask = "   %s\n      %s %u %u %u %u %4u %10.6f %10.6f %10.6f\n\n";
+#define EXAMPLE_FLOAT "%12.9f"
 
-	const uint x = 1280;
-	const uint y = 1024;
+static void printE(const char *descr, const char *p, uint type, uint colorsheme, color iter, float xx, float yy, float zoom){
+	const uint scrx = 1280;
+	const uint scry = 1024;
 
+	static const char *mask1 = "   %s\n";
+	static const char *mask2 = "      %s %u %u %u %u %4u " EXAMPLE_FLOAT " " EXAMPLE_FLOAT " " EXAMPLE_FLOAT "\n\n";
+
+	printf(mask1, descr);
+	printf(mask2, p, scrx, scry, type, colorsheme, iter, xx, yy, zoom);
+}
+
+static void printExamples(const char *p){
 	const color iter =  64;
 	const color iter2 = 256;
 
 	printf("%s\n", "Mandelbrot Examples:");
 
-	printf(mask, "BIG picture",				p, x, y, 0, 0, iter,  -0.500, +0.000, 1.800);
-	printf(mask, "Bulb Mandelbrot",				p, x, y, 0, 0, iter,  -1.770, +0.000, 0.060);
+	printE("BIG picture",					p, 0, 0, iter,  -0.500, +0.000, 1.800);
+	printE("Bulb Mandelbrot",				p, 0, 0, iter,  -1.770, +0.000, 0.060);
 
-	printf(mask, "Elephant valley",				p, x, y, 0, 0, iter2, +0.336, +0.052, 0.012);
-	printf(mask, "Elephant valley (Cosmos colors)",		p, x, y, 0, 2, iter2, +0.336, +0.052, 0.012);
+	printE("Elephant valley",				p, 0, 0, iter2, +0.336, +0.052, 0.012);
+	printE("Elephant valley (Cosmos colors)",		p, 0, 2, iter2, +0.336, +0.052, 0.012);
 
-	printf(mask, "Seahorse valley",				p, x, y, 0, 0, 1024,  -0.747, +0.102, 0.005);
-	printf(mask, "Seahorse valley (Cosmos colors)",		p, x, y, 0, 2, iter2, -0.747, +0.102, 0.005);
+	printE("Seahorse valley",				p, 0, 0, 1024,  -0.747, +0.102, 0.005);
+	printE("Seahorse valley (Cosmos colors)",		p, 0, 2, iter2, -0.747, +0.102, 0.005);
 
-	printf(mask, "Triple spiral",				p, x, y, 0, 0, iter2, -0.041, +0.682, 0.008);
-	printf(mask, "Triple spiral (Cosmos colors)",		p, x, y, 0, 2, iter2, -0.041, +0.682, 0.008);
+	printE("Triple spiral",					p, 0, 0, iter2, -0.041, +0.682, 0.008);
+	printE("Triple spiral (Cosmos colors)",			p, 0, 2, iter2, -0.041, +0.682, 0.008);
 
-	printf(mask, "Imperial Orb valley",			p, x, y, 0, 0, iter2, -1.370, +0.040, 0.018);
+	printE("Imperial Orb valley",				p, 0, 0, iter2, -1.370, +0.040, 0.018);
 
 
 	printf("%s\n", "Burning Ship Examples:");
 
-	printf(mask, "BIG picture",				p, x, y, 1, 0, iter, -0.500, -0.500, 1.800);
+	printE("BIG picture",					p, 1, 0, iter, -0.500, -0.500, 1.800);
 
-	printf(mask, "Burning ship in the armada",		p, x, y, 1, 0, iter2, -1.760, -0.040, 0.060);
-	printf(mask, "Burning ship in the armada (low iter)",	p, x, y, 1, 0, iter, -1.760, -0.040, 0.060);
+	printE("Burning ship in the armada",			p, 1, 0, iter2, -1.760, -0.040, 0.060);
+	printE("Burning ship in the armada (low iter)",		p, 1, 0, iter, -1.760, -0.040, 0.060);
 
-	printf(mask, "Antenna detail",				p, x, y, 1, 0, iter, -1.772, -0.040, 0.010);
-	printf(mask, "Mysterious lady",				p, x, y, 1, 0, iter,  0.000, -1.015, 0.015);
+	printE("Antenna detail",				p, 1, 0, iter, -1.772, -0.040, 0.010);
+	printE("Mysterious lady",				p, 1, 0, iter,  0.000, -1.015, 0.015);
+
+	printE("Hidden treasure",				p, 1, 0, 4096,  -1.73723891, -0.028338253, 0.000000005);
+
+	printE("Hidden Forest",					p, 1, 0, 2048,  -1.737230, -0.028248, 0.00001);
+	printE("Hidden treasure",				p, 1, 0, 2048,  -1.7372348, -0.028248, 0.0000005);
 
 
 	printf("%s\n", "Perpendicular Burning Ship Examples:");
 
-	printf(mask, "BIG picture",				p, x, y, 2, 0, iter2, -0.500000, -0.500000, 1.80000);
+	printE("BIG picture",					p, 2, 0, iter2, -0.500000, -0.500000, 1.80000);
 
-	printf(mask, "First IFS Tree Cardioid",			p, x, y, 2, 0, iter2, -0.064150, +0.649680, 0.00015);
-	printf(mask, "Rhombus inside First IFS Tree Cardioid",	p, x, y, 2, 0, iter2, -0.064154, +0.649703, 0.00001);
-	printf(mask, "Common IFS Tree Cardioid",		p, x, y, 2, 0, iter2, -0.069900, +0.649800, 0.00040);
+	printE("First IFS Tree Cardioid",			p, 2, 0, iter2, -0.064150, +0.649680, 0.00015);
+	printE("Rhombus inside First IFS Tree Cardioid",	p, 2, 0, iter2, -0.064154, +0.649703, 0.00001);
+	printE("Common IFS Tree Cardioid",			p, 2, 0, iter2, -0.069900, +0.649800, 0.00040);
 
-	printf(mask, "Spiral galaxy",				p, x, y, 2, 0, 1024, -0.074000, +0.650710, 0.00030);
+	printE("Spiral galaxy",					p, 2, 0, 1024, -0.074000, +0.650710, 0.00030);
 
-	printf(mask, "Humanoid creature - The Alien",		p, x, y, 2, 0, iter2, -0.079500, +0.659000, 0.00050);
-	printf(mask, "Humanoid creature - The Minotaur",	p, x, y, 2, 0, iter2, -0.081830, +0.649450, 0.00005);
-	printf(mask, "Humanoid creature - The Skull",		p, x, y, 2, 0, iter2, -0.096750, +0.652460, 0.00005);
+	printE("Humanoid creature - The Alien",			p, 2, 0, iter2, -0.079500, +0.659000, 0.00050);
+	printE("Humanoid creature - The Minotaur",		p, 2, 0, iter2, -0.081830, +0.649450, 0.00005);
+	printE("Humanoid creature - The Skull",			p, 2, 0, iter2, -0.096750, +0.652460, 0.00005);
 
-	printf(mask, "Butterflies BIG Picture",			p, x, y, 2, 0, iter2, -0.232180, +0.708370, 0.00100);
-	printf(mask, "Butterfly",				p, x, y, 2, 0, iter2, -0.232180, +0.708370, 0.00005);
+	printE("Butterflies BIG Picture",			p, 2, 0, iter2, -0.232180, +0.708370, 0.00100);
+	printE("Butterfly",					p, 2, 0, iter2, -0.232180, +0.708370, 0.00005);
 
 
 	printf("%s\n", "Conjugate (Symmetrical) Mandelbrot Examples:");
-	printf(mask, "BIG picture",				p, x, y, 3, 0, iter,   0.000000,   0.000000,  2.50000);
-	printf(mask, "Elephant valley in Conjugate Mandelbrot",	p, x, y, 3, 0, 2048,  +1.8699952, +1.8699952, 0.0000003);
+
+	printE("BIG picture",					p, 3, 0, iter,   0.000000,   0.000000,  2.50000);
+	printE("Elephant valley in Conjugate Mandelbrot",	p, 3, 0, 2048,  +1.8699952, +1.8699952, 0.0000003);
 }
 
-void printUsage(const char *p){
-	printf("Mandelbrot Generator\n");
-	printf("\t Copyleft 2015-04, Nikolay Mihaylov, nmmm@nmmm.nu\n");
-	printf("\t License GPLv3+: GNU GPL version 3 or later http://gnu.org/licenses/gpl.html\n");
-	printf("\t This is free software: you are free to change and redistribute it.\n");
-	printf("\n");
+static void printUsage(const char *p){
+	printf("Mandelbrot Generator\n"
+		"\t Copyleft 2015-04, Nikolay Mihaylov, nmmm@nmmm.nu\n"
+		"\t License GPLv3+: GNU GPL version 3 or later http://gnu.org/licenses/gpl.html\n"
+		"\t This is free software: you are free to change and redistribute it.\n"
+		"\n"
+	);
 
 	printf("Usage %s\n", p);
 	printf("\t %-20s - %s\n",	"[scr_x] [scr_y]",	"screen 'resolution' (800x600)");
@@ -99,7 +114,7 @@ void printUsage(const char *p){
 #define GET_BURNINGSHIP(x, y, zoom)	mandelbrot_get(&mpl, SCREEN, MANDELBROT_TYPE_BURNINGSHIP,   COLOR, ITERATIONS, x, y, zoom)
 #define GET_PERPENDICULAR(x, y, zoom)	mandelbrot_get(&mpl, SCREEN, MANDELBROT_TYPE_PERPENDICULAR, COLOR, ITERATIONS, x, y, zoom)
 
-const Mandelbrot *getMandelbrotData(const char **s){
+static const Mandelbrot *getMandelbrotData(const char **s){
 	static Mandelbrot mpl;
 
 	return mandelbrot_get(
